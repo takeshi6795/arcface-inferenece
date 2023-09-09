c07b43d53f09559b5c4f92f42bcbdf217b3bc1ea
takeshi6795 <144521485+takeshi6795@users.noreply.github.com>
2023-09-09 17:42:09 +0900

    init commit

---
+++ deploy/test.py
@@ -0,0 +1,24 @@
+import face_model
+import argparse
+import cv2
+import sys
+import numpy as np
+
+parser = argparse.ArgumentParser(description='face model test')
+# general
+parser.add_argument('--image-size', default='112,112', help='')
+parser.add_argument('--model', default='', help='path to load model.')
+parser.add_argument('--ga-model', default='', help='path to load model.')
+parser.add_argument('--gpu', default=0, type=int, help='gpu id')
+parser.add_argument('--det', default=0, type=int, help='mtcnn option, 1 means using R+O, 0 means detect from begining')
+parser.add_argument('--flip', default=0, type=int, help='whether do lr flip aug')
+parser.add_argument('--threshold', default=1.24, type=float, help='ver dist threshold')
+args = parser.parse_args()
+
+model = face_model.FaceModel(args)
+img = cv2.imread('test.jpg')
+img = model.get_input(img)
+f1 = model.get_feature(img)
+f2 = model.get_feature(img)
+dist = np.sum(np.square(f1-f2))
+print(dist)
